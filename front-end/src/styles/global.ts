d4f324a65bbd7ead84fe7b4aa5c90bd0061fd59b
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2022-11-14 20:49:27 -0300

    add front-end

---
+++ front-end/src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components"
+
+
+export default createGlobalStyle`
+
+
+    * {
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+        font-family: "Inter", sans-serif;
+    }
+
+    :root {}
+
+    body {}
+
+    button {
+        cursor: pointer;
+    }
+
+    a {
+        text-decoration: none;
+    }
+`
