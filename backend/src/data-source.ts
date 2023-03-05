17e2d641a7b2eade89b5f7823c3ec0ee3c0a296b
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-03-05 13:48:16 -0300

    building the backend on the Render platform

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,33 +1,33 @@
 import { DataSource, DataSourceOptions } from "typeorm";
-import { SeederOptions } from "typeorm-extension";
-import { MainSeeder } from "./seeds/main.seeder";
+import "reflect-metadata";
+import path from "path";
+import "dotenv/config";
 
-require("dotenv").config();
+const dataSourceConfig = (): DataSourceOptions => {
+  const entitiesPath: string = path.join(__dirname, "./entities/*.{ts,js}");
+  const migrationsPath: string = path.join(__dirname, "./migrations/*.{ts,js}");
+  const nodeEnv: string | undefined = process.env.NODE_ENV;
 
-const port = process.env.POSTGRES_PORT as number | undefined;
+  if (nodeEnv === "production") {
+    return {
+      type: "postgres",
+      url: process.env.DATABASE_URL,
+      entities: [entitiesPath],
+      migrations: [migrationsPath],
+    };
+  }
 
-const options: DataSourceOptions & SeederOptions = {
-  type: "postgres",
-  host: process.env.POSTGRES_HOST,
-  port: port,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
-  synchronize: false,
-  entities: [`${__dirname}/**/entities/*.{ts,js}`],
-  migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
-  seeds: [MainSeeder],
+  return {
+    type: "postgres",
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    logging: false,
+    entities: [entitiesPath],
+    migrations: [migrationsPath],
+  };
 };
 
-const AppDataSource = new DataSource(
-  process.env.NODE_ENV === "test"
-    ? {
-        type: "sqlite",
-        database: ":memory:",
-        synchronize: true,
-        entities: [`${__dirname}/**/entities/*.{ts,js}`],
-      }
-    : options
-);
-
-export { AppDataSource };
+export const AppDataSource = new DataSource(dataSourceConfig());
